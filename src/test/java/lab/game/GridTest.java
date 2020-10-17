dad62a7700ebc4cb566a7eeea5a16d21029375fc
R.Rajput <r.rajput@hotmail.com>
2020-10-17 16:04:41 +0200

    provide initial state, broken test now. fix coming in next commit

--- src/test/java/lab/game/GridTest.java
+++ src/test/java/lab/game/GridTest.java
@@ -1,5 +1,6 @@
 package lab.game;
 
+import static lab.game.Cell.CellState.ALIVE;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Test;
@@ -11,13 +12,23 @@ public class GridTest {
 
     @Test
     public void should_be_initiated_with_initial_state() {
-        Grid grid = new Grid();
-        Cell[][] state = grid.getState();
-        assertThat(state).isNull();
 
+        Cell.CellState[][] input = new Cell.CellState[][]{
+                {ALIVE, ALIVE, ALIVE, ALIVE, ALIVE, ALIVE, ALIVE, ALIVE},
+                {ALIVE, ALIVE, ALIVE, ALIVE, ALIVE, ALIVE, ALIVE, ALIVE},
+                {ALIVE, ALIVE, ALIVE, ALIVE, ALIVE, ALIVE, ALIVE, ALIVE},
+                {ALIVE, ALIVE, ALIVE, ALIVE, ALIVE, ALIVE, ALIVE, ALIVE}
+        };
+        Grid grid = new Grid(input);
+        Cell[][] state = grid.getState();
+        assertThat(state).isNotNull();
     }
 
     private class Grid {
+        public Grid(Cell.CellState[][] input) {
+
+        }
+
         public Cell[][] getState() {
             return null;
         }
