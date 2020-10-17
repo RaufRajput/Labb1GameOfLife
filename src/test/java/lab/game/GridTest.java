13b54b196780b36b92bd972bb461ff008510aa9b
R.Rajput <r.rajput@hotmail.com>
2020-10-17 16:23:32 +0200

    move out inner Grid class for sake of refactoring

--- src/test/java/lab/game/GridTest.java
+++ src/test/java/lab/game/GridTest.java
@@ -29,28 +29,4 @@ public class GridTest {
         assertThat(state).isEqualTo(input);
     }
 
-    private class Grid {
-        private Cell[][] state;
-
-        public Grid(Cell.CellState[][] input) {
-            state = new Cell[input.length][];
-            for (int row = 0; row < input.length; row++) {
-                state[row] = new Cell[input[row].length];
-                for (int col = 0; col < input[row].length; col++) {
-                    state[row][col] = new Cell(input[row][col]);
-                }
-            }
-        }
-
-        public Cell.CellState[][] getState() {
-            Cell.CellState[][] cellStates = new Cell.CellState[state.length][];
-            for (int row = 0; row < state.length; row++) {
-                cellStates[row] = new Cell.CellState[state[row].length];
-                for (int col = 0; col < state[row].length; col++) {
-                    cellStates[row][col] = state[row][col].getCellSate();
-                }
-            }
-            return cellStates;
-        }
-    }
 }
