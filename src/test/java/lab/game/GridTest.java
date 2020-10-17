8ffc61bca3a49893e80bbcd49518478892cd9c76
R.Rajput <r.rajput@hotmail.com>
2020-10-17 16:20:29 +0200

    initiate board with input, but test still broken. As the getting state still missing.

--- src/test/java/lab/game/GridTest.java
+++ src/test/java/lab/game/GridTest.java
@@ -1,7 +1,9 @@
 package lab.game;
 
 import static lab.game.Cell.CellState.ALIVE;
+import static lab.game.Cell.CellState.DEAD;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.in;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,26 +12,38 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class GridTest {
 
+
+    public static final Cell.CellState X = ALIVE;
+    public static final Cell.CellState O = DEAD;
+
     @Test
     public void should_be_initiated_with_initial_state() {
 
         Cell.CellState[][] input = new Cell.CellState[][]{
-                {ALIVE, ALIVE, ALIVE, ALIVE, ALIVE, ALIVE, ALIVE, ALIVE},
-                {ALIVE, ALIVE, ALIVE, ALIVE, ALIVE, ALIVE, ALIVE, ALIVE},
-                {ALIVE, ALIVE, ALIVE, ALIVE, ALIVE, ALIVE, ALIVE, ALIVE},
-                {ALIVE, ALIVE, ALIVE, ALIVE, ALIVE, ALIVE, ALIVE, ALIVE}
+                {O, O, O, O, O, O, O, O},
+                {O, O, O, O, X, O, O, O},
+                {O, O, O, X, X, O, O, O},
+                {O, O, O, O, O, O, O, O}
         };
         Grid grid = new Grid(input);
-        Cell[][] state = grid.getState();
-        assertThat(state).isNotNull();
+        Cell.CellState[][] state = grid.getState();
+        assertThat(state).isEqualTo(input);
     }
 
     private class Grid {
-        public Grid(Cell.CellState[][] input) {
+        private Cell[][] state;
 
+        public Grid(Cell.CellState[][] input) {
+            state = new Cell[input.length][];
+            for (int row = 0; row < input.length; row++) {
+                state[row] = new Cell[input[row].length];
+                for (int col = 0; col < input[row].length; col++) {
+                    state[row][col] = new Cell(input[row][col]);
+                }
+            }
         }
 
-        public Cell[][] getState() {
+        public Cell.CellState[][] getState() {
             return null;
         }
     }
